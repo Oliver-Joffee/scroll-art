079db46fd8ce01638dc9424b8bafef686106f25d
Mania2134 <oliverjoffee@gmail.com>
2025-10-15 18:24:53 -0400

    Finished second scroll art

--- src/Second.java
+++ src/Second.java
@@ -1,18 +1,51 @@
 public class Second {
-    public static int width = App.getTerminalWidth();
-    public static int count = 0;
-    public static int start = (width/2) - 6;
-    public static void main() {
-        
-        String[][] curvedStar = new String[12][12];
-        App.fillArray(curvedStar);
-        
-        while (true) {
-            if (count % 12 == 1)
+    public static int chars = 2;
+    public static int workingArea = getWorkingArea();
+    public static int spaces = 0;
+    public static boolean receding = true;
 
-            count = count + 1;
+    public static void main(String[] args) {
+        
+        System.err.println(App.getTerminalWidth());
+        System.err.println(getWorkingArea());
+        
+        while(true) {
+            spaces = (workingArea / 2) - (chars /2);
+            for(int i = 0; i < spaces; i++) {
+                System.out.print(" ");
+            }
+
+            for(int i = 0; i < chars; i++) {
+                System.out.print("#");
+            }
+
+            if (chars == workingArea || chars == 2) {
+                receding = !receding;
+            }
+            
+            if(receding) {
+                chars = chars / 2;
+            } else {
+                chars = chars * 2;
+            }
+            System.err.println();
+
         }
     }
 
+    public static int getWorkingArea() {
+        int num = 2;
+        int count = 1;
+        while (true) {
+            for(int i = 0; i < count; i++){
+                num = num * 2;
+            }
+            if ((num * 2) > App.getTerminalWidth()) {
+                return num / 2;
+            } else {
+                count ++;
+            }
+        }
+    }
 
 }
